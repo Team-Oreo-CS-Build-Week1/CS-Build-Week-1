35047ca5293a0af7b869157c0fd20ee4a317a9fc
Leana <leananeparidze.com>
2020-01-08 21:35:13 -0800

    correction

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls import include
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [  
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
-    include(r'^$', views.FrontendAppView.as_view()), 
-    # path(r'^.*/$', views.FrontendAppView.as_view()), 
+    url(r'^', views.FrontendAppView.as_view()), 
+    
 ]
