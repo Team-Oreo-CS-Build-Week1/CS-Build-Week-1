18e39cb1c32e40be6f48e383c34a425a813fa8bf
Leana <leananeparidze.com>
2020-01-08 20:39:04 -0800

    interim

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
-    include(r'^', views.FrontendAppView.as_view()),
+    # include(r'^', views.FrontendAppView.as_view()),
+    path('/', include(views.FrontendAppView.as_view()))
 ]
