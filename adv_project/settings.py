111c0df2cf3a9ebc96ab72a3f78733e667fa4886
Leana <leananeparidze.com>
2020-01-08 20:04:14 -0800

    correction

--- adv_project/settings.py
+++ adv_project/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-REACT_APP_DIR = os.path.join(BASE_DIR, 'react')
+REACT_APP_DIR = os.path.join(BASE_DIR, 'react/frontend')
 
 STATICFILES_DIRS= [
     os.path.join(REACT_APP_DIR, 'build', 'static'),
