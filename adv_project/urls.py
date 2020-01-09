c1a2bcd8edde6000dd36d4d643d5afdaf1e55e0d
Leana <leananeparidze.com>
2020-01-08 20:57:39 -0800

    interim

--- adv_project/urls.py
+++ adv_project/urls.py
@@ -3,10 +3,10 @@ from django.urls import path, include
 from django.conf.urls import include
 from . import views
 
-urlpatterns = [
-    include(r'^', views.FrontendAppView.as_view()),   
+urlpatterns = [  
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/adv/', include('adventure.urls')),
+    include(r'^.*/$', views.FrontendAppView.as_view()), 
     
 ]
